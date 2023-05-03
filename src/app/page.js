7be078d699f9b7bb9357a55622ae4a70821b3dec
vibe008 <104353180+vibe008@users.noreply.github.com>
2023-05-03 12:31:30 +0530

    first commit

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,17 @@
 
 import { Inter } from 'next/font/google'
-import v1 from '../../public/images/bugativ.mp4'
+import v1 from '../../public/images/down1.jpg'
+import Image from 'next/image'
 export default function Home() {
   return (
 <>
-      <video
+      {/* <video
         src={v1}
         controls
         width={640}
         height={360}
-      />
+      /> */}
+      <Image src={v1} alt="" />
 </>
   )
 }
